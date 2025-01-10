a234a36c6c130135fa8b4f0b5aff03a6856253fa
Fabio G Guedes <105892163+investfgg@users.noreply.github.com>
2025-01-10 17:04:57 -0300

    Atualização main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import somaResultado from './scripts/somaResultado.js';
-import ehFibonacci from './scripts/ehFibonacci.js';
-import contasStatus from './scripts/contasStatus.js';
-import fatMensal from './scripts/fatMensal.js';
-import palavraInvertida from './scripts/palavraInvertida.js';
+import somaResultado from './somaResultado.js';
+import ehFibonacci from './ehFibonacci.js';
+import contasStatus from './contasStatus.js';
+import fatMensal from './fatMensal.js';
+import palavraInvertida from './palavraInvertida.js';
 
 somaResultado();
 ehFibonacci();
