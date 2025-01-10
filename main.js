741daf4784f20ea43291b73ce81f5ab0866e894e
Fabio G Guedes <105892163+investfgg@users.noreply.github.com>
2025-01-10 16:56:59 -0300

    Inserindo novo trabalho

---
+++ main.js
@@ -0,0 +1,11 @@
+import somaResultado from './scripts/somaResultado.js';
+import ehFibonacci from './scripts/ehFibonacci.js';
+import contasStatus from './scripts/contasStatus.js';
+import fatMensal from './scripts/fatMensal.js';
+import palavraInvertida from './scripts/palavraInvertida.js';
+
+somaResultado();
+ehFibonacci();
+contasStatus();
+fatMensal();
+palavraInvertida();
